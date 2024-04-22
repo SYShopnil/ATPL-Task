966cc1e25bb761cb84a286430012ed598ea68a6e
Shah Sadmaney Yeasar <sadmanishopnil@gmail.com>
2024-04-22 06:03:04 +0600

    slide change

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,6 +1,6 @@
 import { Authorization } from "@root/lib/authorization";
 
 export default async function ProfilePage() {
-  await Authorization(["admin", "user"]);
+  // await Authorization(["admin", "user"]);
   return <div>I am from page of profile under dashboard</div>;
 }
